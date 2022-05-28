93d0e86184dd924d55a0d4a98e0a0ba7fd514100
RAFALAMAO <aaronjt@outlook.com>
2022-05-28 13:52:05 -0500

    Add Env Bearer

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,12 +1,14 @@
 const API = "https://api.themoviedb.org/3";
 
+const bearer = process.env.REACT_APP_MOVIE_API_KEY;
+
 export function httpGet(path) {
     return fetch(API + path, {
       method: "GET",
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmN2EyMTQ3Yzc2ODM5ZjUwNzlhMzViMjc0YWFkOWYwMiIsInN1YiI6IjYyNjA3YzQ2NjNhYWQyMDA1MWNmZjVlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wNx7Q_TJuXT9N-NzqSY1REU3-BjM1JPaJ_FP2jeYgpA",
-        "Content-Type": "application/json;charset=utf-8"
+          `Bearer ${bearer}`,
+          "Content-Type": "application/json;charset=utf-8"
         }
     }).then(result => result.json());
 }
